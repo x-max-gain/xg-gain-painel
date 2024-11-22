e8907152a756615b9249f7a18609a353515dc37f
lauricioweb <lauriciomalek@gmail.com>
2024-11-22 16:02:38 -0300

    carrousel parte 1

---
+++ src/app/(app)/(platform)/painel/bot/page.tsx
@@ -0,0 +1,8 @@
+export default function Bot(){
+ return (
+  <div className="w-full h-72">
+  <h4>bot pagina </h4>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit atque nisi, culpa, sed laudantium error libero tempora eligendi id fuga ex? Iure fuga vel facilis quidem provident dolor, doloribus fugiat.</p>
+</div>
+ )
+}
--- src/app/(app)/(platform)/painel/company/[id]/page.tsx
+++ src/app/(app)/(platform)/painel/company/[id]/page.tsx
@@ -46,7 +46,7 @@ export default function Company() {
             {/* VÍDEOS */}
             <div className="bg-background-primary rounded-md mt-4">
               <h2 className="px-4 py-2"><strong>Vídeos</strong></h2>
-              <div className="flex px-4 overflow-x-hidden">
+              <div className="flex px-4 " style={{ overflowX: "auto", maxWidth: "9999px" }}>
                <Carrousel/>
               </div>
             </div>
